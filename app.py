0f23f7f1c93f8b8cead559e9e995a019bced7375
Nahid <mdnhossain11@gmail.com>
2022-03-29 16:01:36 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from os import getenv
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+app.run(
+    host=getenv("IP", "0.0.0.0"),
+    port=int(getenv("PORT", "8080"))
+)
